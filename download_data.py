28fbd3913411d221513f5decce7d362d99457e53
mns-gpu <n.li@uva.nl>
2022-02-01 10:17:18 +0000

    Add more file ids

--- download_data.py
+++ download_data.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 def download_file_from_google_drive(id, destination):
     URL = "https://docs.google.com/uc?export=download"
@@ -29,7 +30,13 @@ def save_response_content(response, destination):
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
 
+output_path = './pytorch/data/github/'
+files = {'1FYfNZKNUXaYTKuOhwY3POhnUGpnQuZOe': 'use.name.h5', 
+         '1yQwqzx7hW7qMGnRDOolI_vYZDNN0_SoA': 'use.tokens.h5',
+         '11Iia3ZyNuD-TYeymo8I3hU1YTNweY9_i': 'use.apiseq.h5'
+}
 if __name__ == "__main__":
-    file_id = '1xpUXsSFbULYEAs8low5zQZWK7-wmqTNO'
-    destination = './step4000000.h5'
-    download_file_from_google_drive(file_id, destination)
+    for key in files.keys(): 
+        file_id = key
+        destination = os.path.join(output_path, files[key])
+        download_file_from_google_drive(file_id, destination)
